cbf5af8b5d5d9c95d7a69f5d361c09c07bd7bfce
Tom <Tom@DESKTOP-P6F14QO>
2023-12-21 23:39:57 -0500

    github page build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-J6B3PCOG.mjs";import"./chunk-B4O42HSN.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-V5P6C3EA.mjs";import"./chunk-B4O42HSN.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
